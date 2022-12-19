de5bf9e487ca1f6a65183bf2a11702270fe2a8bc
zapalblizh <zapal.blizh@gmail.com>
2022-12-19 04:48:05 -0500

    finished task

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -59,6 +59,10 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
+            <div style={{ marginLeft: "2rem" }}>
+                <span>{currency}</span>
+            </div>    
+
                     <input
                         required='required'
                         type='number'
@@ -72,7 +76,7 @@ const AllocationForm = (props) => {
                         Save
                     </button>
                 </div>
-                </div>
+            </div>
 
         </div>
     );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,39 +1,84 @@
-import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../context/AppContext";
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+  const { dispatch, currency } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
+    dispatch({
+      type: "RED_EXPENSE",
+      payload: expense,
+    });
+  };
 
-    }
-
-
-    return (
-        <tr>
+  return (
+    <>
+      <style type="text/css">{`
+	  .btn-container {
+			display: flex;
+			align-items: center;
+            justify-content: center;
+			color: #FFFFFF;
+			width: 2rem;
+			height: 2rem;
+			font-size: 1rem;
+			border-radius: 30px;
+	  }
+	  .btn-container.increase {
+		background-color: #4EAC5A;
+	  }
+	  .btn-container.increase:hover {
+		cursor: pointer;
+	  }
+	  .btn-container.decrease {
+		background-color: #B12519;
+	  }
+	  .btn-container.decrease:hover {
+		cursor: pointer;
+	  }
+	  `}</style>
+      <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+        <td>
+          {currency}
+          {props.cost}
+        </td>
+        <td>
+          <div class="btn-container increase" onClick={(event) => increaseAllocation(props.name)}><h1>+</h1></div>
+        </td>
+        <td>
+          <div class="btn-container decrease" onClick={(event) => decreaseAllocation(props.name)}><h1>-</h1></div>
+        </td>
+        <td>
+          <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+        </td>
+      </tr>
+    </>
+  );
 };
 
 export default ExpenseItem;
